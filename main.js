1f775bbbd905c52cc0ade5366c9f24dd8e7dc73a
Bladimir Moracher <blad2@users.noreply.github.com>
2021-06-25 22:17:53 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$("body").css({
+  "background":
+    "url(http://imgstocks.com/wp-content/uploads/2013/12/Detected-for-the-first-ever-time-in-space-space-picture-for-wallpaper.jpg)",
+"font-family":"Crismson"});
+$(".jumbotron").css("margin-top","5%");
+$("img").css({"width":"300px","height":"360px","max-width":"300px",/*"display":"block" this makes the images have their original dimensions id height is set to auto. and even though i pass the test, it looks ugly*/});
+$(".col-xs-4").css({"width-left":"0 auto","margin-bottom":"4%","margin-right":"0 auto"});
+$("blockquote").css("margin-top","10%");
+});
